6272282a75051bda49ec0af53dd8331dca9edf2c
Shisam1012 <shsa10121999@gmail.com>
2024-10-15 17:33:50 +0300

    Add login screen and modify css styling

--- client/src/App.js
+++ client/src/App.js
@@ -2,28 +2,30 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomeScreen from './components/HomeScreen';
 import Question from './components/Question';
+import Login from './components/Login/Login';
 
 import './App.css';
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <div id='background-div'>
-        <div className='content-container'>
-          <img
-            src='/images/triviaMasterHeader3.png'
-            width='87%'
-            height='34%'
-            alt='trivia master'
-          />
-          <Routes>
-            <Route path='/' element={<HomeScreen />} />
-            <Route path='/questions' element={<Question />} />
-          </Routes>
-        </div>
-      </div>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <div id='background-div'>
+                <div className='content-container'>
+                    <img
+                        src='/images/triviaMasterHeader3.png'
+                        width='87%'
+                        height='34%'
+                        alt='trivia master'
+                    />
+                    <Routes>
+                        <Route path='/' element={<Login />} />
+                        <Route path='/HomeScreen' element={<HomeScreen />} />
+                        <Route path='/Question' element={<Question />} />
+                    </Routes>
+                </div>
+            </div>
+        </BrowserRouter>
+    );
 };
 
 export default App;
